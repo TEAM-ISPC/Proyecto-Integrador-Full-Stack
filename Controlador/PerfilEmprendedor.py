3562122d42aa6872acd84bc5465446d8862ed3bf
PabloAndresJerez <106399054+PabloAndresJerez@users.noreply.github.com>
2022-10-17 07:25:39 -0300

    Agrego controlador Perfil Emprendedor

---
+++ Controlador/PerfilEmprendedor.py
@@ -0,0 +1,16 @@
+""" 
+Url = /PerfilEmprendedor
+
+
+En este controlador mostrariamos el turnero al emprendedor con los datos necesarios para su organización. 
+Tambien traerá los datos del emprendedor con la posiblidad de editarlos.
+
+
+Posee los siguiente métodos:
+    - AdministrarAgenda
+    - EditarPerdil
+    - GuardarPerfil
+
+Consume datos de los modulos Usuario, Emprendedor y Turnos.
+
+""" 
