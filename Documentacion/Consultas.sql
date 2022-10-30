92e3a04b45ae009f64c4025e455e9ea8642cb326
Emanuel Luna <34133110+memaluna@users.noreply.github.com>
2022-10-30 16:32:06 -0300

    Se agregaron consulta de Tabla Usuario

--- Documentacion/Consultas.sql
+++ Documentacion/Consultas.sql
@@ -1,1 +1,3 @@
-
+-- Tabla Usuario
+INSERT INTO usuarios (idUsuario, apellido, nombre, email, password, telefono) VALUES (0, "Campos", "Emanuel", "email@com.com", 1234, "351241214");
+SELECT * FROM usuarios
