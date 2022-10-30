6bcd1304e4219da8f524405d0ea811e4b50decc4
PabloAndresJerez <106399054+PabloAndresJerez@users.noreply.github.com>
2022-10-30 18:40:47 -0300

    Agrego aclaración

--- Documentacion/Consultas.sql
+++ Documentacion/Consultas.sql
@@ -1,4 +1,4 @@
 -- Tabla Usuario
 INSERT INTO usuarios (idUsuario, apellido, nombre, email, password, telefono) VALUES (0, "Campos", "Emanuel", "email@com.com", 1234, "351241214");
 SELECT * FROM usuarios
-hellow word
+-- Continuar aqui
