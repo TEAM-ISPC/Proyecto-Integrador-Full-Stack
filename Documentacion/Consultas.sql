f746509fa720a626261617467c6391823cf42733
Exequiel2k <89609660+Exequiel2k@users.noreply.github.com>
2022-11-05 17:53:20 -0300

    agregan usuarios

--- Documentacion/Consultas.sql
+++ Documentacion/Consultas.sql
@@ -1,4 +1,10 @@
 -- Tabla Usuario
 INSERT INTO usuarios (idUsuario, apellido, nombre, email, password, telefono) VALUES (0, "Campos", "Emanuel", "email@com.com", 1234, "351241214");
-SELECT * FROM usuarios
+INSERT INTO usuarios (idUsuario, apellido, nombre, email, password, telefono) VALUES (0, "rojas", "marcos", "marcos@com.com", 'marcos2k', "3523092919");
+INSERT INTO usuarios (idUsuario, apellido, nombre, email, password, telefono) VALUES (0, "cardelli", "yohana", "yohana@com.com", 'yoha2020', "353412231");
+INSERT INTO usuarios (idUsuario, apellido, nombre, email, password, telefono) VALUES (0, "garelli", "mauricio", "mauricio@com.com",'mauri2k', "351231235");
+INSERT INTO usuarios (idUsuario, apellido, nombre, email, password, telefono) VALUES (0, "salas", "santiago", "santiago@com.com", 'dog2022', "355421313");
+SELECT * FROM usuarios;
+SELECT idUsuario, nombre FROM usuarios;
+SELECT * FROM usuarios where nombre = 'yohana';
 -- Continuar aqui
